8ea3ef8f74765dc3bdd4c0fc8dc645fc550b294a
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-21 21:55:26 +0100

    OMG It's sort of working

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,20 +1,233 @@
-// LinkedList.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "Header.h"
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <cstddef>
+
+
+using namespace std;
+
+template <typename ItemType>
+class LinkedList
+{
+public:
+    LinkedList();
+    ~LinkedList();
+
+    void Add(const ItemType& item);
+    void MultiAdd(const vector<ItemType>& items);
+    ItemType Get(uint32_t index) const;
+    void Set(uint32_t index, const ItemType& item);
+    ItemType Remove(const ItemType& item);
+    void Clear();
+    uint32_t Count() const;
+    bool Contains(const ItemType& item) const;
+    uint32_t IndexOf(const ItemType& item) const; // declaration of IndexOf function
+
+
+private:
+    struct Node
+    {
+        Node* next{ nullptr };
+        Node* previous{ nullptr };
+        ItemType data{};
+
+        Node(const ItemType& item, Node* nextNode = nullptr, Node* previousNode = nullptr)
+            : next(nextNode), previous(previousNode), data(item)
+        {
+        }
+    };
+
+
+    Node* firstNode{ nullptr };
+    Node* lastNode{ nullptr };
+
+    Node* makeNode(const ItemType& item);
+    Node* getNodeAt(uint32_t index) const;
+};
+
+
+template <typename ItemType>
+LinkedList<ItemType>::LinkedList() : firstNode(nullptr), lastNode(nullptr) {}
+
+template <typename ItemType>
+LinkedList<ItemType>::~LinkedList()
+{
+    Clear();
+}
+
+
+template <typename ItemType>
+typename LinkedList<ItemType>::Node* LinkedList<ItemType>::makeNode(const ItemType& item)
+{
+    Node* node = new Node{ item, nullptr, nullptr };
+    return node;
+}
+
+template <typename ItemType>
+void LinkedList<ItemType>::Add(const ItemType& item)
+{
+    Node* node = makeNode(item);
+
+    if (firstNode == nullptr)
+    {
+        firstNode = node;
+        lastNode = node;
+    }
+    else
+    {
+        lastNode->next = node;
+        node->previous = lastNode;
+        lastNode = node;
+    }
+}
+
+template <typename ItemType>
+void LinkedList<ItemType>::MultiAdd(const vector<ItemType>& items)
+{
+    for (const ItemType& item : items)
+    {
+        Add(item);
+    }
+}
+
+template <typename ItemType>
+typename LinkedList<ItemType>::Node* LinkedList<ItemType>::getNodeAt(uint32_t index) const
+{
+    Node* current = firstNode;
+    for (uint32_t i = 0; i < index; i++)
+    {
+        if (current == nullptr)
+        {
+            throw out_of_range("Index out of range");
+        }
+        current = current->next;
+    }
+    return current;
+}
+
+template <typename ItemType>
+ItemType LinkedList<ItemType>::Get(uint32_t index) const
+{
+    Node* node = getNodeAt(index);
+    return node->data;
+}
+
+template <typename ItemType>
+void LinkedList<ItemType>::Set(uint32_t index, const ItemType& item)
+{
+    Node* node = getNodeAt(index);
+    node->data = item;
+}
+
+template <typename ItemType>
+ItemType LinkedList<ItemType>::Remove(const ItemType& item)
+{
+    Node* current = firstNode;
+    while (current != nullptr)
+    {
+        if (current->data == item)
+        {
+            (current == firstNode ? firstNode : current->previous->next) = current->next;
+            (current == lastNode ? lastNode : current->next->previous) = current->previous;
+
+            ItemType removedItem = current->data;
+            delete current;
+            current = nullptr;
+            return removedItem;
+        }
+        current = current->next;
+    }
+
+    // item not found
+    return ItemType{};
+}
+
+template <typename ItemType>
+void LinkedList<ItemType>::Clear()
+{
+    Node* current = firstNode;
+    while (current != nullptr)
+    {
+        Node* next = current->next;
+        delete current;
+        current = next;
+    }
+    firstNode = nullptr;
+    lastNode = nullptr;
+}
+
+template <typename ItemType>
+uint32_t LinkedList<ItemType>::IndexOf(const ItemType& item) const
+
+{
+    Node* current = firstNode;
+    int index = 0;
+    while (current != nullptr)
+    {
+        if (current->data == item) return index;
+        current = current->next;
+        ++index;
+    }
+    return -1;
+}
+
+
+template <typename ItemType>
+bool LinkedList<ItemType>::Contains(const ItemType& item) const
+{
+    Node* current = firstNode;
+    while (current != nullptr)
+    {
+        if (current->data == item) return true;
+
+        current = current->next;
+    }
+    return false;
+}
+
+template <typename ItemType>
+uint32_t LinkedList<ItemType>::Count() const
+{
+    uint32_t count = 0;
+    Node* current = firstNode;
+    while (current != nullptr)
+    {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    LinkedList<int> myLinkedList;
+    myLinkedList.Add(1);
+    myLinkedList.Add(2);
+    myLinkedList.Add(3);
+    cout << "Linked List contains: " << myLinkedList.Count() << " elements." << endl;
+
+    int firstItem = myLinkedList.Get(0);
+    cout << "First Item: " << firstItem << endl;
+
+    int removedItem = myLinkedList.Remove(2);
+    cout << "Removed Item: " << removedItem << endl;
+
+    myLinkedList.Set(1, 4);
+
+    int index = myLinkedList.IndexOf(4);
+    if (index != -1) {
+        cout << "Item found at index: " << index << endl;
+    }
+
+    bool containsItem = myLinkedList.Contains(3);
+    if (containsItem) {
+        cout << "Linked List contains the item 3." << endl;
+    }
+
+    myLinkedList.Clear();
+    cout << "Linked List cleared." << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- LinkedListTests/LinkedListTests.cpp
+++ LinkedListTests/LinkedListTests.cpp
@@ -1,16 +1,153 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "F:\Game Programming\211-cpp-OOP-nice1stu\LinkedList\LinkedList.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
-namespace LinkedListTests
+namespace LinkedListTest
 {
-	TEST_CLASS(LinkedListTests)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-		}
-	};
+    TEST_CLASS(UnitTestLinkedList)
+    {
+    private:
+        LinkedList<int> list;
+
+    public:
+        void SetUpStdList()
+        {
+            list.Add(13);
+            list.Add(5);
+            list.Add(100);
+            list.Add(101);
+            list.Add(69);
+        }
+
+        void ClearStdList()
+        {
+            list.Clear();
+        }
+
+        TEST_METHOD_INITIALIZE(SetUp)
+        {
+            SetUpStdList();
+        }
+
+        TEST_METHOD_CLEANUP(TearDown)
+        {
+            ClearStdList();
+        }
+
+        TEST_METHOD(TestAddOne)
+        {
+            Assert::AreEqual(list.Count(), 5u, L"List size should be 5", LINE_INFO());
+            list.Add(4);
+            Assert::AreEqual(list.Count(), 6u, L"List size should be 6", LINE_INFO());
+        }
+
+        TEST_METHOD(TestEmptyList)
+        {
+            ClearStdList();
+            Assert::AreEqual(list.Count(), 0u, L"List size should be 0", LINE_INFO());
+        }
+
+        TEST_METHOD(TestMultiAdd)
+        {
+            vector<int> items{ 0, -2, 54 };
+            list.MultiAdd(items);
+            Assert::AreEqual(list.Count(), 8u, L"List size should be 8", LINE_INFO());
+        }
+
+        TEST_METHOD(TestGetItemFromCorrectIndex)
+        {
+            Assert::AreEqual(list.Get(2), 100, L"Item at index 1 should be 100", LINE_INFO());
+        }
+
+        TEST_METHOD(TestSetIteminCorrectIndex)
+        {
+            list.Set(3, 999);
+            Assert::AreEqual(list.Get(3), 999, L"Item at index 3 should be 999", LINE_INFO());
+        }
+
+        TEST_METHOD(TestCountIsZeroAfterClear)
+        {
+            list.Clear();
+            Assert::AreEqual(list.Count(), 0u, L"List size should be 0", LINE_INFO());
+        }
+
+        TEST_METHOD(TestRemoveIndexAt)
+        {
+            list.Remove(1);
+            Assert::AreEqual(list.Count(), 4u, L"List size should be 4", LINE_INFO());
+            Assert::AreEqual(list.Get(1), 101, L"Item at index 1 should be 101", LINE_INFO());
+        }
+
+        TEST_METHOD(TestContainsValueAtCorrectIndex)
+        {
+            Assert::IsTrue(list.Contains(5), L"List should contain 5", LINE_INFO());
+            Assert::IsFalse(list.Contains(4), L"List should not contain 4", LINE_INFO());
+        }
+
+        TEST_METHOD(IndexOf_ExistingItem_ReturnsCorrectIndex)
+        {
+            int itemToFind = 100;
+            uint32_t index = list.IndexOf(itemToFind);
+            Assert::AreEqual((uint32_t)2, index);
+        }
+
+        TEST_METHOD(IndexOf_FirstItem_ReturnsCorrectIndex)
+        {
+            int itemToFind = 13;
+            uint32_t index = list.IndexOf(itemToFind);
+            Assert::AreEqual((uint32_t)0, index);
+        }
+
+        TEST_METHOD(IndexOf_LastItem_ReturnsCorrectIndex)
+        {
+            int itemToFind = 69;
+            uint32_t index = list.IndexOf(itemToFind);
+            Assert::AreEqual((uint32_t)4, index);
+        }
+
+        TEST_METHOD(IndexOf_NonExistingItem_ReturnsMinusOne)
+        {
+            int itemToFind = 123;
+            uint32_t index = list.IndexOf(itemToFind);
+            Assert::AreEqual((uint32_t)-1, index);
+        }
+
+        TEST_METHOD(Remove_ExistingItem_RemovesItemFromList)
+        {
+            int itemToRemove = 100;
+            int removedItem = list.Remove(itemToRemove);
+
+            Assert::AreEqual(itemToRemove, removedItem);
+            Assert::IsFalse(list.Contains(itemToRemove));
+        }
+
+        TEST_METHOD(Remove_FirstItem_RemovesItemFromList)
+        {
+            int itemToRemove = 13;
+            int removedItem = list.Remove(itemToRemove);
+
+            Assert::AreEqual(itemToRemove, removedItem);
+            Assert::IsFalse(list.Contains(itemToRemove));
+        }
+
+        TEST_METHOD(Remove_LastItem_RemovesItemFromList)
+        {
+            int itemToRemove = 69;
+            int removedItem = list.Remove(itemToRemove);
+
+            Assert::AreEqual(itemToRemove, removedItem);
+            Assert::IsFalse(list.Contains(itemToRemove));
+        }
+
+        TEST_METHOD(Remove_NonExistingItem_ReturnsDefaultValue)
+        {
+            int itemToRemove = 123;
+            int removedItem = list.Remove(itemToRemove);
+
+            Assert::AreEqual(0, removedItem);
+        }
+    };
 }
