b9abebec7502d51421e18ce9b574eab24e6b2a9f
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-28 01:44:51 +0200

    create header.h and unit tests

---
+++ DynamicArrayTests/DynamicArrayTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DynamicArrayTests
+{
+	TEST_CLASS(DynamicArrayTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
