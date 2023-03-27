6ccd87d4997c2ae8570c73eaba5dd5189951ce92
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-28 00:31:57 +0200

    Update StringClassExercise.cpp
    removed the dynamic array class

--- StringClassExercise/StringClassExercise.cpp
+++ StringClassExercise/StringClassExercise.cpp
@@ -6,52 +6,9 @@
 
 using namespace std;
 
-template<typename T>
-class DynamicArray {
-private:
-    T* data;
-    size_t size;
-    size_t count;
+class StringClassExercise;
 
-public:
-    DynamicArray(size_t count) : data(new T[count]), size(0), count(count) {}
-
-    ~DynamicArray()
-    {
-        delete[] data;
-    }
-
-    void add(const T& element)
-    {
-        if (size == count) resize();
-
-        data[size++] = element;
-    }
-
-    const T& get(size_t index) const
-    {
-        return (index >= size) ? throw std::out_of_range("Index out of range!") : data[index];
-    }
-
-    size_t getSize() const
-    {
-        return size;
-    }
-
-private:
-    void resize()
-    {
-        count *= 2;
-        T* newData = new T[count];
-        for (size_t i = 0; i < size; ++i)
-        {
-            newData[i] = data[i];
-        }
-        delete[] data;
-        data = newData;
-    }
-};
-
+ostream& operator<<(ostream& os, const StringClassExercise& string);
 
 class StringClassExercise
 {
@@ -59,6 +16,65 @@ private:
     size_t length;
     char* buffer;
     size_t maxSize;
+
+public:
+    StringClassExercise();
+    StringClassExercise(size_t maxSize);
+    StringClassExercise(const char* defaultText, size_t maxSize);
+    StringClassExercise(const StringClassExercise& other);
+    StringClassExercise(StringClassExercise&& other) noexcept;
+    ~StringClassExercise();
+    StringClassExercise& operator=(const StringClassExercise& other);
+    StringClassExercise& operator=(StringClassExercise&& other) noexcept;
+    bool operator==(const StringClassExercise& other) const;
+    void append(const char* text);
+    void appendLine(const char* text);
+    void print() const;
+    const char* getString() const;
+};
+
+ostream& operator<<(ostream& os, const StringClassExercise& string);
+
+int main()
+{
+    try
+    {
+        StringClassExercise joinText(20);
+        joinText.append("Foo");
+        joinText.appendLine("Bar");
+        cout << "text1: ";
+        joinText.print();
+
+        StringClassExercise defaultText("The quick brown fox", 25);
+        defaultText.append(" jumped over the lazy dog");
+        cout << "text2: " << defaultText.getString() << endl;
+
+        StringClassExercise tooLongText(5);
+        tooLongText.append(">How much wood");
+        tooLongText.appendLine("longer text");
+        cout << "text3: " << tooLongText.getString() << endl;
+
+        StringClassExercise copiedText = defaultText;
+        cout << "text4: " << copiedText.getString() << endl;
+
+        StringClassExercise movedText = move(tooLongText);
+        cout << "text5: " << movedText.getString() << endl;
+
+        try
+        {
+            joinText.append("This string is too long");
+        }
+        catch (const exception& e)
+        {
+            cerr << "Error: " << e.what() << endl;
+        }
+    }
+    catch (const exception& e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
+    return 0;
+}
 
 public:
     StringClassExercise() : buffer(new char[256]), length(0), maxSize(256)
@@ -162,45 +178,4 @@ public:
 ostream& operator<<(ostream& os, const StringClassExercise& string)
     os << string.getString();
     return os;
-}
-
-int main()
-{
-    try
-    {
-        StringClassExercise joinText(20);
-        joinText.append("Foo");
-        joinText.appendLine("Bar");
-        cout << "text1: ";
-        joinText.print();
-
-        StringClassExercise defaultText("The quick brown fox", 25);
-        defaultText.append(" jumped over the lazy dog");
-        cout << "text2: " << defaultText.getString() << endl;
-
-        StringClassExercise tooLongText(5);
-        tooLongText.append(">How much wood");
-        tooLongText.appendLine("longer text");
-        cout << "text3: " << tooLongText.getString() << endl;
-
-        StringClassExercise copiedText = defaultText;
-        cout << "text4: " << copiedText.getString() << endl;
-
-        StringClassExercise movedText = move(tooLongText);
-        cout << "text5: " << movedText.getString() << endl;
-
-        try
-        {
-            joinText.append("This string is too long");
-        }
-        catch (const exception& e)
-        {
-            cerr << "Error: " << e.what() << endl;
-        }
-    }
-    catch (const exception& e)
-    {
-        cerr << "Error: " << e.what() << endl;
-    }
-    return 0;
 }
