53abcd746e951debf5d16b91f0ccb90c867cac6c
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-28 00:24:58 +0200

    created Dynamic Array as it's own class, not part of string class

---
+++ DynamicArrayClass/DynamicArrayClass.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstring>
+#include <stdexcept>
+#include <cstdio>
+
+using namespace std;
+
+template<typename T>
+class DynamicArray
+{
+    private:
+        T* data;
+        size_t size;
+        size_t count;
+
+    public:
+        DynamicArray(size_t count) : data(new T[count]), size(0), count(count) {}
+
+        ~DynamicArray()
+        {
+            delete[] data;
+        }
+
+        void add(const T& element)
+        {
+            size == count ? resize() : void();
+            data[++size] = element;
+        }
+
+
+        const T& get(size_t index) const
+        {
+            return (index >= size) ? throw std::out_of_range("Index out of range!") : data[index];
+        }
+
+        size_t getSize() const
+        {
+            return size;
+        }
+
+    private:
+        void resize()
+        {
+            count *= 2;
+            T* newData = new T[count];
+            for (size_t i = 0; i < size; ++i)
+            {
+                newData[i] = data[i];
+            }
+            delete[] data;
+            data = newData;
+        }
+};
