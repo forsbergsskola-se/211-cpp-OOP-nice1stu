db3f1015a1f4210142355c486a77cb78d6f4d075
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-28 00:20:15 +0200

    Update StringClassExercise.cpp
    just realised i did dynamic array wrong

--- StringClassExercise/StringClassExercise.cpp
+++ StringClassExercise/StringClassExercise.cpp
@@ -6,16 +6,15 @@
 
 using namespace std;
 
-template <typename T>
-class DynamicArray
-{
+template<typename T>
+class DynamicArray {
 private:
     T* data;
     size_t size;
     size_t count;
 
 public:
-    DynamicArray(size_t maxSize) : data(new T[maxSize]), size(0), maxSize(maxSize) {}
+    DynamicArray(size_t count) : data(new T[count]), size(0), count(count) {}
 
     ~DynamicArray()
     {
@@ -24,19 +23,35 @@ public:
 
     void add(const T& element)
     {
-        size == maxSize ? resize() : data[++size] = element;
+        if (size == count) resize();
+
+        data[size++] = element;
     }
 
     const T& get(size_t index) const
     {
-        return (index >= size) ? throw out_of_range("Index out of range!") : data[index];
+        return (index >= size) ? throw std::out_of_range("Index out of range!") : data[index];
     }
 
     size_t getSize() const
     {
         return size;
     }
+
+private:
+    void resize()
+    {
+        count *= 2;
+        T* newData = new T[count];
+        for (size_t i = 0; i < size; ++i)
+        {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+    }
 };
+
 
 class StringClassExercise
 {
